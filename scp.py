fb3bd62ee2c38efe1c9ac09562c4133bc1e4df41
胡一鳞 <hu10xiaorui@outlook.com>
2019-07-15 15:24:12 -0700

    finish gui, fix bugs

--- scp.py
+++ scp.py
@@ -6,12 +6,12 @@ from random import randint as rand
 appid = sys.argv[1]
 itemname = sys.argv[2]
 
-print('''curl "https://steamcommunity.com/market/priceoverview/?appid='''+appid+'''&currency=1&market_hash_name=''' + itemname +'''"''')
-pricejson = os.popen('''curl "https://steamcommunity.com/market/priceoverview/?appid=730&currency=1&market_hash_name=''' + itemname +'''"''').read() # example Sticker%20|%20Kawaii%20Killer%20Terrorist
-print(pricejson)
+#print('''curl "https://steamcommunity.com/market/priceoverview/?appid='''+appid+'''&currency=1&market_hash_name=''' + itemname +'''"''')
+pricejson = os.popen('''curl "https://steamcommunity.com/market/priceoverview/?appid='''+appid+'''&currency=1&market_hash_name=''' + itemname +'''"''').read() # example Sticker%20|%20Kawaii%20Killer%20Terrorist
+#print(pricejson)
 pricejson = json.loads(pricejson)
-print("Price is",pricejson["lowest_price"])
-print("=========================================================================")
-print(pricejson["lowest_price"].replace("$",""))
+#print("Price is",pricejson["lowest_price"])
+#print("=========================================================================")
+#print(pricejson["lowest_price"].replace("$",""))
 priceincents = pricejson["lowest_price"].replace("$","").replace(".","")
 print(priceincents)
--- scpgui.py
+++ scpgui.py
@@ -1,24 +1,70 @@
-def test():
-    print("sadfasdfasdfasdf")
+def rstart():
+    thread = threading.Thread(target=start, args=())
+    thread.daemon = True                            # Daemonize thread
+    thread.start()
+def start():
+
+
+
+    stop = False
+    print(gameappid.get())
+    print(itemname.get())
+    print(timebc.get())
+
+    if stop == True:
+        pass
+    else:
+        price=getprice(gameappid.get(),itemname.get())
+        startprice = price
+        while(1):
+
+
+            if stop == True:
+                break
+            price=getprice(gameappid.get(),itemname.get())
+            print(price)
+            if startprice != price:
+                if startprice > price:
+                    print("sdafasFDasfda")
+                else:
+                    print("sdaf")
+            tk.after(int(timebc.get())*1000)
+
+def getprice(appid,itemname):
+    pricejson = os.popen('''curl "https://steamcommunity.com/market/priceoverview/?appid='''+appid+'''&currency=1&market_hash_name=''' + itemname +'''"''').read()
+    # example CSGO 730 Sticker%20|%20Kawaii%20Killer%20Terrorist PUBG 578080 %5BBATTLESTAT%5D%20Industrial%20Security%20-%20AKM
+    pricejson = json.loads(pricejson)
+    priceincents = pricejson["lowest_price"].replace("$","").replace(".","").replace(",","")
+    return priceincents
+def stopr():
+    global stop
+    if stop == True:
+        tk.destroy()
+        quit()
+    stop=True
+import threading
+import os
+import json
 import tkinter
+stop=True
 #from tkinter.constants import *
 tk = tkinter.Tk()
 tk.title("Steam Community Price Checker")
 label = tkinter.Label(text="Steam Community Price Checker")
 label.place(x=0,y=0)
-tk.geometry("360x260")
+tk.geometry("480x190")
 e5=tkinter.Label(tk,text='''Steam game's appid:''').place(x=0,y=30)
 e6=tkinter.Label(tk,text='''Item's full name:''').place(x=0,y=70)
-e7=tkinter.Label(tk,text='''Time between each check:''').place(x=0,y=110)
-v1 = tkinter.StringVar()
-v2 = tkinter.StringVar()
-v3 = tkinter.StringVar()
-
-e1 = tkinter.Entry(tk,textvariable=v1)
-e2 = tkinter.Entry(tk,textvariable=v2)   #用*号代替用户输入的内容
-e3 = tkinter.Entry(tk,textvariable=v3)
-e1.place(x=160,y=30)
-e2.place(x=160,y=70)
-e3.place(x=160,y=110)
-b2=tkinter.Button(tk,text='OK',command=test).place(x=20,y=150)
+e7=tkinter.Label(tk,text='''Seconds between each check:''').place(x=0,y=110)
+gameappid = tkinter.StringVar()
+itemname = tkinter.StringVar()
+timebc = tkinter.StringVar()
+e1 = tkinter.Entry(tk,textvariable=gameappid)
+e2 = tkinter.Entry(tk,textvariable=itemname)   #用*号代替用户输入的内容
+e3 = tkinter.Entry(tk,textvariable=timebc)
+e1.place(x=220,y=30)
+e2.place(x=220,y=70)
+e3.place(x=220,y=110)
+b2=tkinter.Button(tk,text='OK',command=rstart).place(x=20,y=150)
+b3=tkinter.Button(tk,text='Cancel',command=stopr).place(x=80,y=150)
 tk.mainloop()
