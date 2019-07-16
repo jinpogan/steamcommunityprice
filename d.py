ca54c16fd371097637f952d924a5e5381d1dd520
胡一鳞 <hu10xiaorui@outlook.com>
2019-07-16 15:38:15 -0700

    fix bugs

---
+++ d.py
@@ -0,0 +1,1 @@
+#label = tkinter.Label(frame, text="SCP will now run in background, it will remind you when "+itemname.get()+"'s ")
--- scpgui.py
+++ scpgui.py
@@ -6,7 +6,6 @@ def pickaduiofile():
     if file_extension == ".mp3":
         sound = AudioSegment.from_mp3("./alarm.mp3")
         sound.export("./alarm.wav", format="wav")
-
 def alarmt(text,balarm):
     if balarm != True:
         wave_obj = sa.WaveObject.from_wave_file("./alarm.wav")
@@ -27,6 +26,7 @@ def alarmt(text,balarm):
             turtle.bgcolor(rand(0,255), rand(0,255), rand(0,255))
             time.sleep(0.5)
 def start():
+    tk.destroy()
     print(gameappid.get())
     print(itemname.get())
     print(timebc.get())
@@ -57,6 +57,10 @@ def start():
         time.sleep(int(timebc.get()))
 
 def getprice(appid,itemname):
+    itemname=urllib.parse.quote(str(itemname)).replace("★","%E2%98%85")
+
+    #print("=============",itemname)
+    #print("s;dkfl;",'''curl "https://steamcommunity.com/market/priceoverview/?appid='''+appid+'''&currency=1&market_hash_name=''' + itemname +'''"''')
     pricejson = os.popen('''curl "https://steamcommunity.com/market/priceoverview/?appid='''+appid+'''&currency=1&market_hash_name=''' + itemname +'''"''').read()
     # example CSGO 730 Sticker%20|%20Kawaii%20Killer%20Terrorist PUBG 578080 %5BBATTLESTAT%5D%20Industrial%20Security%20-%20AKM
     pricejson = json.loads(pricejson)
@@ -72,13 +76,13 @@ from pydub import AudioSegment
 import simpleaudio as sa
 from shutil import copy2 as cp
 from tkinter import filedialog
-
-#from tkinter.constants import *
+import urllib.parse
+from tkinter.constants import *
 tk = tkinter.Tk()
 tk.title("Steam Community Price Checker")
 label = tkinter.Label(text="Steam Community Price Checker")
 label.place(x=0,y=0)
-tk.geometry("480x320")
+tk.geometry("480x340")
 e5=tkinter.Label(tk,text='''Steam game's appid:''').place(x=0,y=30)
 e6=tkinter.Label(tk,text='''Item's full name:''').place(x=0,y=70)
 e7=tkinter.Label(tk,text='''Seconds between each check:''').place(x=0,y=110)
@@ -96,4 +100,7 @@ e2.place(x=220,y=70)
 e3.place(x=220,y=110)
 b2=tkinter.Button(tk,text='OK',command=start).place(x=20,y=190)
 b3=tkinter.Button(tk,text='Quit',command=tk.destroy).place(x=80,y=190)
+#Exterior: Field-Tested
+help=tkinter.Message(tk,width=480,text='''If you are checking csgo prices,ou have include the exterior. For example,if you want to check ★ Flip Knife | Autotronic's with the field tested exterior. You should enter ★ Flip Knife | Autotronic (Field-Tested). If this still doesn't work, you should make the item name url friendly. For example: ★ Flip Knife | Autotronic (Field-Tested) will be %E2%98%85%20Flip%20Knife%20%7C%20Autotronic%20%28Field-Tested%29.    !!!App may freeze after you click start.!!!''').place(x=0,y=210)
+
 tk.mainloop()
